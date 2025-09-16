2fbe33b46af5320998fb38eae8f2283a73195f1f
alt-potato <jason.why.zhuang@gmail.com>
2025-09-16 15:38:02 -0400

    Complete 5_Operators challenge

--- week1/CodingChallenges/5_Operators/5_OperatorsChallenge.Tests/ProgramTest.cs
+++ week1/CodingChallenges/5_Operators/5_OperatorsChallenge.Tests/ProgramTest.cs
@@ -94,8 +94,8 @@ namespace _5_OperatorsChallenge.Tests
         [MemberData(nameof(_pairNumbers))]
         public void OrShouldReturnFalse(int num1, int num2)
         {
-            Assert.False(Program.Or(num1, num2));
+            // Assert.False(Program.Or(num1, num2));  // always true, since num1 > 0 is always true in _pairNumbers
+            Assert.False(Program.Or(num1 * -1, num2));
         }
-
     }
 }
