fe110bd83e86480d08bcb86d76548d66bf51ab57
alt-potato <jason.why.zhuang@gmail.com>
2025-09-18 11:02:41 -0400

    Add initial TodoApp meterials

--- week1/TodoApp/Program.cs
+++ week1/TodoApp/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Serilog;
+
+namespace TodoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Error.WriteLine("initializing...");
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            // serilog - logging for .NET apps
+            //   logging sinks (console, file, etc.)
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+            builder.Host.UseSerilog();
+            
+            var app = builder.Build();
+
+            app.MapGet("/", (ILogger<Program> logger) =>
+            {
+                logger.LogInformation("Hello World endpoint was called");
+                return "Hello World!";
+            });
+
+            app.Run();
+
+            Console.Error.WriteLine("started!");
+        }
+    }
+}
